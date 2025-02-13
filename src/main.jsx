1935b9be7058ee9f611d0d8a64f41493c791804f
Isaiah Duhart <isaiah.duhart.1@gmail.com>
2025-02-13 12:17:58 -0500

    Updates to add cookies on the backend, calling necessary apis with credentials included, retrying fetching profile while backend waits for spotify to callback

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import routes from "./routes";
 const router = createBrowserRouter(routes);
 
 createRoot(document.getElementById("root")).render(
-  // <StrictMode>
+  <StrictMode>
     <RouterProvider router={router} />
-  // </StrictMode>
+  </StrictMode>
 );
